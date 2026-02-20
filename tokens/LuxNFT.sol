7bb762816f025778b0d7a80d215443bfdc1b39b8
Shh12308 <164091255+Shh12308@users.noreply.github.com>
2026-02-20 21:24:22 +0000

    Implement LuxNFT contract with minting function

--- tokens/LuxNFT.sol
+++ tokens/LuxNFT.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract LuxNFT is ERC721, Ownable {
+    uint256 public nextTokenId;
+
+    constructor() ERC721("LuxNFT", "LUXNFT") {}
+
+    function mint(address to) external onlyOwner {
+        _safeMint(to, nextTokenId);
+        nextTokenId++;
+    }
+}
